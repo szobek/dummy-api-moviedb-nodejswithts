c97084936d5dba1d47e25df04dd3b53d85eb8fc3
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-22 21:21:11 +0200

    Refactor movie interface and update movies retrieval endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,44 +2,29 @@ import { Router, Request, Response } from 'express';
 import db from '../config/knex'; // Importáljuk a Knex példányt
 
 const router = Router();
-
-// Tegyük fel, hogy van egy 'users' táblánk
-interface User {
+interface Movie{
   id: number;
-  name: string;
-  email: string;
+  title: string;
+  description: string;
+  year: number;
+  rating: number;
+  director: string;
+  genres: string;
+  actors: string;
+  poster_url: string;
 }
-
 // GET /api/users - Összes felhasználó lekérdezése
 router.get('/movies', async (req: Request, res: Response) => {
   try {
-    const users: User[] = await db<User>('movies').select('*');
-    res.json(users);
+    const movies: Movie[] = await db<Movie>('movies').select('*');
+    res.json(movies);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Hiba történt a filmeklekérdezése során.' });
   }
 });
 
-// POST /api/users - Új felhasználó létrehozása
-router.post('/users', async (req: Request, res: Response) => {
-    try {
-        const { name, email } = req.body;
-        if (!name || !email) {
-            return res.status(400).json({ message: 'A név és az email megadása kötelező.' });
-        }
-        
-        const newUser = { name, email };
 
-        const [userId] = await db<User>('users').insert(newUser);
-
-        res.status(201).json({ id: userId, ...newUser });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Hiba történt a felhasználó létrehozása során.' });
-    }
-});
 
 
 export default router;
