8b71203b9c3c760344acb24681d1dac849ef9a15
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-25 15:12:22 +0200

    Add CORS support to the application and update dependencies in package.json and package-lock.json

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import movieApiRoutes from '../src/routes/movie_api_routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
-
 // Útvonalak beillesztése
 app.use('/api/movies', movieApiRoutes);
 
