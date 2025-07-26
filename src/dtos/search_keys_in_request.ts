5cf2caef779ade749967796b98afa31b1a27194c
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-07-26 11:17:32 +0200

    Refactor search parameters in SearcKeysInRequestDto to use snake_case and update related comments in request.http

--- src/dtos/search_keys_in_request.ts
+++ src/dtos/search_keys_in_request.ts
@@ -1,18 +1,18 @@
 export default class SearcKeysInRequestDto {
   title?: string;
-  fromyear?: number;
-  toyear?: number;
+  from_year?: number;
+  to_year?: number;
   rating?: number;
   actor?: string;
   genre?: number;
 
   constructor(params: any) {
     this.title = (params.title || "%").trim();
-    this.fromyear = params.fromyear
-      ? parseInt(params.fromyear)
+    this.from_year = params.fromyear
+      ? parseInt(params.from_year)
       : 1950;
-    this.toyear =
-      params.toyear? parseInt(params.toyear) :
+    this.to_year =
+      params.toyear? parseInt(params.to_year) :
       parseInt(new Date().getFullYear().toString());
     this.rating = params.rating ? parseInt(params.rating) : 1;
     this.actor = (params.actor || "%").trim()
